32908837a798d261f03364496e60bb44a9aaae33
pierreEdimo <pedimonkoe@yahoo.com>
2021-06-17 17:56:09 +0200

    add a add_save_button

---
+++ lib/widgets/add_save_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:noted/models/books.dart';
+import 'package:noted/models/steaky_notes.dart';
+import 'package:noted/services/note_service.dart';
+import 'package:noted/widgets/show_snack.dart';
+import 'package:provider/provider.dart';
+
+Widget saveButton(TextEditingController controller, int id,
+    BuildContext context, Books books) {
+  return Container(
+    alignment: Alignment.bottomRight,
+    child: IconButton(
+      icon: Icon(
+        Icons.save_outlined,
+        color: Colors.white,
+      ),
+      onPressed: () {
+        if (controller.text.isNotEmpty) {
+          SteakyNote newNote = SteakyNote(
+            content: controller.text,
+            creationDate: DateTime.now(),
+            booKey: id,
+          );
+
+          Provider.of<NoteService>(context, listen: false).createNote(newNote);
+
+          controller.clear();
+
+          showSnack(context, "Saved");
+        }
+        DoNothingAction();
+      },
+    ),
+  );
+}
