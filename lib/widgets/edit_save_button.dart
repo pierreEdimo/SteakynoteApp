e6fedee3cb47a6d2cc3269f91790cc08fd1214eb
pierreEdimo <pedimonkoe@yahoo.com>
2021-06-17 17:54:58 +0200

    add edit_save_button

---
+++ lib/widgets/edit_save_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:noted/models/steaky_notes.dart';
+import 'package:noted/services/note_service.dart';
+import 'package:noted/widgets/show_snack.dart';
+import 'package:provider/provider.dart';
+
+Widget editSaveButton(
+  TextEditingController controller,
+  BuildContext context,
+  SteakyNote note,
+) {
+  return IconButton(
+    onPressed: () {
+      SteakyNote editedNote = note;
+      editedNote.content = controller.text;
+      editedNote.editedDate = DateTime.now();
+
+      Provider.of<NoteService>(context, listen: false).editNote(editedNote);
+
+      showSnack(context, "Edits saved");
+    },
+    icon: Icon(
+      Icons.save_outlined,
+      color: Colors.white,
+    ),
+  );
+}
