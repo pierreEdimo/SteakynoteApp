1d24153688323d6704a19a8b515dd348ef570eef
pierreEdimo <pedimonkoe@yahoo.com>
2021-06-17 17:52:35 +0200

    add  markdown preview screen

---
+++ lib/widgets/preview_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:noted/services/Editing_service.dart';
+import 'package:provider/provider.dart';
+
+Widget reviewButton(TextEditingController controller, BuildContext context) {
+  return IconButton(
+    onPressed: () {
+      Provider.of<EditingService>(
+        context,
+        listen: false,
+      ).addtext(controller.text);
+
+      Navigator.of(context).pushNamed('/preview');
+    },
+    icon: Icon(
+      Icons.remove_red_eye_outlined,
+      color: Colors.white,
+    ),
+  );
+}
